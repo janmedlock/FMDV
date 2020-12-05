dfeb4dd475a4d18615d4a89179be89e273ca2341
Jan Medlock <jan.medlock@oregonstate.edu>
2020-12-05 13:53:36 -0800

    Make one figure for each model.

--- initial_conditions.py
+++ initial_conditions.py
@@ -55,7 +55,7 @@ def load():
     return (infected, et)
 
 
-def plot_infected(ax, infected, model, SAT, IC, label_model, draft=False):
+def plot_infected(ax, infected, model, SAT, IC, draft=False):
     # .unstack('run') puts 'run' on columns, time on rows.
     i = infected.loc[(model, SAT, IC)].unstack('run')
     if draft:
@@ -86,12 +86,7 @@ def plot_infected(ax, infected, model, SAT, IC, label_model, draft=False):
                                  labelleft=False, labelright=False)
         ax.yaxis.offsetText.set_visible(False)
     if ax.is_first_row():
-        title = f'Initial condition {IC}'
-        if label_model:
-            model_ = {'chronic': 'carrier'}.get(model, model)
-            title = '\n'.join((f'{model_.capitalize()} model',
-                               title))
-        ax.set_title(title, loc='center')
+        ax.set_title(f'Initial condition {IC}', loc='center')
 
 
 def kdeplot(endog, ax=None, shade=False, cut=0, **kwds):
@@ -159,71 +154,68 @@ def plot(infected, et, draft=False):
     rc['axes.labelsize'] = 8
     rc['axes.titlesize'] = 9
     nrows = len(SATs) * 2
-    ncols = len(models) * len(ICs)
+    ncols = len(ICs)
     height_ratios = (3, 1) * (nrows // 2)
     width_ratios = (1, ) * ncols
     with seaborn.axes_style('whitegrid'), pyplot.rc_context(rc=rc):
-        fig = pyplot.figure(constrained_layout=True)
-        gs = fig.add_gridspec(nrows, ncols,
-                              height_ratios=height_ratios,
-                              width_ratios=width_ratios)
-        axes = numpy.empty((nrows, ncols), dtype=object)
-        axes[0, 0] = None  # Make sharex & sharey work for axes[0, 0].
-        for row in range(nrows):
-            for (j, model) in enumerate(models):
-                col_model = j * len(ICs)
-                for (k, IC) in enumerate(ICs):
-                    col = col_model + k
-                    # Models share the x scale.
-                    sharex = axes[0, col_model]
+        # Common upper y limit for infected plots.
+        infected_max = infected.max() * (1 + pyplot.rcParams['axes.ymargin'])
+        for model in models:
+            fig = pyplot.figure(constrained_layout=True)
+            gs = fig.add_gridspec(nrows, ncols,
+                                         height_ratios=height_ratios,
+                                         width_ratios=width_ratios)
+            axes = numpy.empty((nrows, ncols), dtype=object)
+            axes[0, 0] = None  # Make sharex & sharey work for axes[0, 0].
+            for row in range(nrows):
+                for (col, IC) in enumerate(ICs):
+                    # Share the x scale.
+                    sharex = axes[0, 0]
                     # The infection plots share the y scale.
                     # The extinction-time plots do *not* share the y scale.
                     sharey = axes[0, 0] if (row % 2 == 0) else None
                     axes[row, col] = fig.add_subplot(gs[row, col],
                                                      sharex=sharex,
                                                      sharey=sharey)
-        IC_mid = ICs[len(ICs) // 2]
-        for (i, SAT) in enumerate(SATs):
-            row_i = 2 * i
-            row_e = 2 * i + 1
-            for (j, model) in enumerate(models):
-                for (k, IC) in enumerate(ICs):
-                    col = j * len(ICs) + k
-                    label_model = (IC == IC_mid)
-                    plot_infected(axes[row_i, col], infected, model, SAT, IC,
-                                  label_model, draft=draft)
-                    plot_extinction_time(axes[row_e, col], et, model, SAT, IC)
-        # Shade time region from acute-model column
-        # in chronic-model column.
-        if 'chronic' in models:
-            e_acute = et.loc['acute']
-            assert e_acute.notnull().all()
-            e_acute_mask = e_acute.max(level=['SAT', 'initial_conditions'])
-            color = pyplot.rcParams['grid.color']
             for (i, SAT) in enumerate(SATs):
-                for row in range(2 * i, 2 * i + 2):
-                    for (j, IC) in enumerate(ICs):
-                        col_chronic = len(ICs) + j
-                        ax = axes[row, col_chronic]
-                        ax.axvspan(0, e_acute_mask[(SAT, IC)],
-                                   color=color, alpha=0.5, linewidth=0)
-        # I get weird results if I set these limits individually.
-        for (i, SAT) in enumerate(SATs):
-            for col in range(ncols):
-                for row in (2 * i, 2 * i + 1):
-                    ax = axes[row, col]
-                    ax.set_xlim(left=0)
-                    ax.set_ylim(bottom=0)
-        seaborn.despine(fig=fig, top=True, right=True,
-                        bottom=False, left=False)
-        # For some reason, aligning the rows and columns works better
-        # than aligning all axes.
-        fig.align_xlabels(axes[-1, :])
-        for (i, SAT) in enumerate(SATs):
-            row_i = 2 * i
-            row_e = 2 * i + 1
-            fig.align_ylabels(axes[[row_i, row_e], 0])
-        # fig.savefig('initial_conditions.pdf')
+                row_i = 2 * i
+                row_e = 2 * i + 1
+                for (col, IC) in enumerate(ICs):
+                    plot_infected(axes[row_i, col], infected, model, SAT, IC,
+                                  draft=draft)
+                    plot_extinction_time(axes[row_e, col], et, model, SAT, IC)
+            # Shade time region from plot of acute model
+            # in plot of chronic model.
+            if model == 'chronic':
+                e_acute = et.loc['acute']
+                assert e_acute.notnull().all()
+                e_acute_mask = e_acute.max(level=['SAT', 'initial_conditions'])
+                color = pyplot.rcParams['grid.color']
+                for (i, SAT) in enumerate(SATs):
+                    for row in range(2 * i, 2 * i + 2):
+                        for (col, IC) in enumerate(ICs):
+                            ax = axes[row, col]
+                            ax.axvspan(0, e_acute_mask[(SAT, IC)],
+                                       color=color, alpha=0.5, linewidth=0)
+            # I get weird results if I set these limits individually.
+            for (i, SAT) in enumerate(SATs):
+                for col in range(ncols):
+                    for row in (2 * i, 2 * i + 1):
+                        ax = axes[row, col]
+                        ax.set_xlim(left=0)
+                        ax.set_ylim(bottom=0)
+                        if row == 2 * i:
+                            ax.set_ylim(top=infected_max)
+            seaborn.despine(fig=fig, top=True, right=True,
+                            bottom=False, left=False)
+            # For some reason, aligning the rows and columns works better
+            # than aligning all axes.
+            fig.align_xlabels(axes[-1, :])
+            for (i, SAT) in enumerate(SATs):
+                row_i = 2 * i
+                row_e = 2 * i + 1
+                fig.align_ylabels(axes[[row_i, row_e], 0])
+            # fig.savefig(f'initial_conditions_{model}.pdf')
 
 
 if __name__ == '__main__':
